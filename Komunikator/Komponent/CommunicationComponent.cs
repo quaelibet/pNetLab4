7b056da1cc7a929f3c3ba8f6227dfd276b7045ae
quaelibet <quaelibet@gmail.com>
2012-11-29 17:08:29 +0100

    project skeleton

---
+++ Komunikator/Komponent/CommunicationComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Komponent
+{
+    public partial class CommunicationComponent : Component
+    {
+        public CommunicationComponent()
+        {
+            InitializeComponent();
+        }
+
+        public CommunicationComponent(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
